fe39f24eccb90f76b9b508bc53d499931715a63d
Pawan Chaugule <pchaugule@anl.gov>
2024-05-31 16:01:44 -0500

    Weibull_3P damage.py

--- srlife/damage.py
+++ srlife/damage.py
@@ -1516,8 +1516,6 @@ class PIAModel(CrackShapeIndependent):
         # Material parameters
         suvals = material.threshold_surf(temperatures)
         svals = material.strength_surf(temperatures)
-        mvals = material.modulus_surf(temperatures)
-        kvals = svals ** (-mvals)
         N = material.Ns(temperatures)
         B = material.Bs(temperatures)
 
@@ -1526,8 +1524,6 @@ class PIAModel(CrackShapeIndependent):
 
         # Temperature average values
         suavg = np.mean(suvals, axis=0)[:count_surface_elements]
-        mavg = np.mean(mvals, axis=0)[:count_surface_elements]
-        kavg = np.mean(kvals, axis=0)[:count_surface_elements]
         Navg = np.mean(N, axis=0)[:count_surface_elements]
         Bavg = np.mean(B, axis=0)[:count_surface_elements]
 
@@ -1567,16 +1563,6 @@ class PIAModel(CrackShapeIndependent):
         surf_pstress_0 -= suavg[..., None, None]
         surf_pstress_0[surf_pstress_0 < 0] = 0
 
-        # # Summing up over last two axes i.e. over the elements of surface stress and
-        # # surfaces for which normals are there
-
-        # # Log reliability in each element
-        # log_reliability = (
-        #     -kavg
-        #     * np.sum(surf_pstress_0 ** mavg[..., None, None], axis=(-1, -2))
-        #     * surface_areas
-        # )
-
         return surf_pstress_0
     
     def calculate_surface_flaw_element_log_reliability(
@@ -1607,27 +1593,16 @@ class PIAModel(CrackShapeIndependent):
         """
 
         # Material parameters
-        # suvals = material.threshold_surf(temperatures)
         svals = material.strength_surf(temperatures)
         mvals = material.modulus_surf(temperatures)
         kvals = svals ** (-mvals)
-        # N = material.Ns(temperatures)
-        # B = material.Bs(temperatures)
 
         # Count number of surface elements
         count_surface_elements = np.count_nonzero(surface)
 
         # Temperature average values
-        # suavg = np.mean(suvals, axis=0)[:count_surface_elements]
         mavg = np.mean(mvals, axis=0)[:count_surface_elements]
         kavg = np.mean(kvals, axis=0)[:count_surface_elements]
-        # Navg = np.mean(N, axis=0)[:count_surface_elements]
-        # Bavg = np.mean(B, axis=0)[:count_surface_elements]
-
-        # Principal stresses in surface elements
-        surf_pstress = self.calculate_surface_principal_stress(
-            mandel_stress, surface, normals
-        )
 
         surf_pstress_0 = self.calculate_surface_time_dep_pstress(
             time, mandel_stress, surface, normals, temperatures, surface_areas, material, tot_time,
@@ -1784,7 +1759,7 @@ class WNTSAModel(CrackShapeIndependent):
 
         return sigma_avg
 
-    def calculate_surface_flaw_avg_normal_stress(
+    def calculate_surface_flaw_time_dep_normal_stress(
         self,
         time,
         mandel_stress,
@@ -1925,6 +1900,40 @@ class WNTSAModel(CrackShapeIndependent):
 
         integral = ((sigma_n_0 ** mavg[..., None, None, None]) * self.ddelta) / (
             2 * np.pi
+        )
+
+        return integral
+    
+    def calculate_surface_flaw_avg_normal_stress(
+        self,
+        time,
+        mandel_stress,
+        surface_elements,
+        surface_normals,
+        temperatures,
+        material,
+        tot_time,
+    ):
+        """
+        Calculate the average normal tensile stresses from the pricipal stresses
+
+        Parameters:
+          mandel_stress:  element stresses in Mandel convention
+          temperatures:   element temperatures
+          material:       material model object with required data that includes
+                          Weibull scale parameter (svals), Weibull modulus (mvals)
+                          and fatigue parameters (Bv,Nv)
+          tot_time:       total service time used as input to calculate reliability
+        """
+
+        integral = self.calculate_surface_flaw_time_dep_normal_stress(
+            time,
+            mandel_stress,
+            surface_elements,
+            surface_normals,
+            temperatures,
+            material,
+            tot_time,
         )
 
         # Flatten the last axis and calculate the mean of the positive values along that axis
